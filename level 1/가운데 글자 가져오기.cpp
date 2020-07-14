53a61b12d11d55f9095fa746adff74c0a89faeff
SANGPHA <68215551+SANG-PHA@users.noreply.github.com>
2020-07-14 11:21:33 +0900

    -

---
+++ level 1/가운데 글자 가져오기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    
+    int size = s.size() / 2;
+    
+    if(s.size() % 2 == 0) // 짝수일 때
+    {
+        answer += s[size - 1];
+        answer += s[size];
+    }        
+    else // 홀수일 때
+        answer += (s[size]);
+    
+    return answer;
+}
