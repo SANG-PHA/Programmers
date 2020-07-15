1580f483c014afb0e649bff8537b93bfde18411c
SANGPHA <68215551+SANG-PHA@users.noreply.github.com>
2020-07-15 16:45:44 +0900

    -

--- level 1/정수 내림차순으로 배치하기.java
+++ level 1/정수 내림차순으로 배치하기.java
@@ -6,12 +6,11 @@ class Solution {
         ArrayList<Long> partial = new ArrayList<>(); // 각 자릿수 숫자를 담을 ArrayList
         
         // 자릿수 별로 쪼개기
-        while(n >= 10) // 10의 자릿수 이상 일때
+        while(n > 0) // 10의 자릿수 이상 일때
         {
             partial.add(n % 10); // 나머지연산을 이용해 1의 자릿수부터 추가
             n = n / 10;
         }
-        partial.add(n); // 1의 자릿수만 남았을 때
         
         // 내림차순 정렬
         Collections.sort(partial);
