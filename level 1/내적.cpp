4e26030a1948acaa429f036dcf7fb630a9f90dc6
SANGPHA <68215551+SANG-PHA@users.noreply.github.com>
2021-03-20 22:32:00 +0900

    EZ

--- level 1/내적.cpp
+++ level 1/내적.cpp
@@ -7,7 +7,7 @@ int solution(vector<int> a, vector<int> b) {
     int answer = 0;
     
     for(int i = 0; i < a.size(); i++)
-        answer += a[i] * b[i];
+        answer += a[i] * b[i]; // 벡터의 내적 연산
     
     return answer;
 }
