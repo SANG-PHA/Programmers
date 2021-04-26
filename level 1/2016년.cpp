04ed6529a4554dfcc9c133eed292b210983510a3
SANGPHA <68215551+SANG-PHA@users.noreply.github.com>
2021-04-26 22:44:11 +0900

    -

--- level 1/2016년.cpp
+++ level 1/2016년.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 string solution(int a, int b) {
     int TheNumberOfDays[12] = {31,29,31,30,31,30,31,31,30,31,30,31}; // x월의 마지막 일
-    string date[7] = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"}; // 요일 - 원형 큐로 구현
+    string date[7] = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"}; // 요일 - 원형 큐로 구현함
     
     int idx_date = 0; // 원형 큐 인덱스
     for(int i = 1; i < a; i ++) // a월 전까지의 요일 계산
